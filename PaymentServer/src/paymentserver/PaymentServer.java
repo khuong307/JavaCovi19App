9f30e04c482b82960c310821049352b8eef50b9c
ddk992001 <63284880+ddk992001@users.noreply.github.com>
2021-12-20 10:47:34 +0700

    Layout Payment App Server

---
+++ PaymentServer/src/paymentserver/PaymentServer.java
@@ -0,0 +1,12 @@
+package paymentserver;
+
+import paymentserver.HomePage.*;
+
+public class PaymentServer {
+
+    public static void main(String[] args) {
+        ServerHomePage hp = new ServerHomePage();
+        hp.show();
+    }
+    
+}
